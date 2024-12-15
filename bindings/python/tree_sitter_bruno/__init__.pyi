7c5ea5ae6a9f4870feba65c357c0122a0709e994
Kristofers Solo <dev@kristofers.xyz>
2024-12-15 17:05:56 +0200

    feat(queries): add tags and folds

--- bindings/python/tree_sitter_bruno/__init__.pyi
+++ bindings/python/tree_sitter_bruno/__init__.pyi
@@ -5,6 +5,6 @@ from typing import Final
 HIGHLIGHTS_QUERY: Final[str]
 INJECTIONS_QUERY: Final[str]
 # LOCALS_QUERY: Final[str]
-# TAGS_QUERY: Final[str]
+TAGS_QUERY: Final[str]
 
 def language() -> object: ...
